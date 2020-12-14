e1fde042c294b5551868aa6d8900f486588f4918
ajmainankon <ajmain.ankon@outlook.com>
2020-12-14 19:38:23 +0600

    first

---
+++ build/precache-manifest.846065492d20e4d6e2cb638ffdf3ade2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58091d3a63cde0f07328c5c542ba3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39a00e56111273d97b48",
+    "url": "/static/css/2.a17a1ed7.chunk.css"
+  },
+  {
+    "revision": "477962afddcb41734221",
+    "url": "/static/css/main.a73b22eb.chunk.css"
+  },
+  {
+    "revision": "39a00e56111273d97b48",
+    "url": "/static/js/2.e5f82656.chunk.js"
+  },
+  {
+    "revision": "8b5cf78b94afdc6b5f957e4eab32c1e8",
+    "url": "/static/js/2.e5f82656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477962afddcb41734221",
+    "url": "/static/js/main.9903d858.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
